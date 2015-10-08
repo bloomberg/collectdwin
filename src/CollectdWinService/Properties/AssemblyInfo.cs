ef3e658069aba963e23107320ef2e6a2c02627e6
Yoga Ramalingam <yramalingam1@bloomberg.net>
2015-10-08 17:34:18 -0400

    Bumped version to 0.5.3.13 by Jenkins

--- src/CollectdWinService/Properties/AssemblyInfo.cs
+++ src/CollectdWinService/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.5.3.12")]
-[assembly: AssemblyFileVersion("0.5.3.12")]
+[assembly: AssemblyVersion("0.5.3.13")]
+[assembly: AssemblyFileVersion("0.5.3.13")]
